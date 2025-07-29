c5275dc6c3f9c449ad38bb45903442d7d6b6afc9
Md. Hanan Sajid <mdhanansjd+github@gmail.com>
2025-07-29 22:57:05 +0530

    project file directory reformatting

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,3 +7,28 @@ module.exports = {
     plugins: [],
   }
   
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html", // making sure it covers all my HTML files
+  ],
+  theme: {
+    extend: {
+      animation: {
+        'fade-in': 'fadeIn 1s ease-out forwards',
+        'fade-out': 'fadeOut 0.5s ease-in forwards',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+        fadeOut: {
+          '0%': { opacity: '1' },
+          '100%': { opacity: '0' },
+        },
+      },
+    },
+  },
+  plugins: [],
+}
